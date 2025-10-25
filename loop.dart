8253848bc0385e625c9d39e93830150306f8fec4
HasibSheikh1222 <hasibsk1614@gmail.com>
2025-10-25 13:57:22 +0600

    first commit

---
+++ loop.dart
@@ -0,0 +1,12 @@
+main() {
+  for (int i = 0; i <= 100; i++) {
+    if (i == 50) {
+      continue;
+    }
+    if (i == 50) {
+      break;
+    }
+    print('Dokane gasi $i');
+  }
+  print('Loop is done');
+}
