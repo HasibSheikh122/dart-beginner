91f45ff06b320e3f9bd4df27822df672fd37db40
HasibSheikh1222 <hasibsk1614@gmail.com>
2025-10-26 15:17:09 +0600

    first commit

---
+++ functions.dart
@@ -0,0 +1,14 @@
+main() {
+  // Function call
+  welcomeMessage("Hasib");
+  welcomeMessage("Abdullah");
+  welcomeMessage("Najib");
+  welcomeMessage("Masum");
+}
+
+// Function
+welcomeMessage(String name) {
+  // Body
+  print('Hi Good morning $name');
+  print('How are you? $name');
+}
