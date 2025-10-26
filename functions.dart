8a20fdaa55fa2e2edd0f878fd2d336ca24221987
HasibSheikh1222 <hasibsk1614@gmail.com>
2025-10-26 16:41:00 +0600

    first commit

--- functions.dart
+++ functions.dart
@@ -7,7 +7,9 @@ main() {
 }
 
 // Function
-welcomeMessage(String name) {
+// Optional Perameter [String address = '', int age = 0]
+// Optional Prrameter non Sequence {String address = '', int age = 0}
+welcomeMessage(String name, [String address = '', int age = 0]) {
   // Body
   print('Hi Good morning $name');
   print('How are you? $name');
