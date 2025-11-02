5c95193f8aba3251c87b0fa0e81f152435300eb7
HasibSheikh1222 <hasibsk1614@gmail.com>
2025-11-02 16:01:22 +0600

    first commit

---
+++ inheritance.dart
@@ -0,0 +1,27 @@
+class Human {
+  int hands;
+  int legs;
+  Human(this.hands, this.legs);
+
+  void moving() {
+    print('Moving');
+  }
+
+  void eating() {
+    print('Eating');
+  }
+}
+
+class Student extends Human {
+  String institute;
+  String section;
+
+  Student(int hands, int legs, this.institute, this.section)
+    : super(hands, legs);
+}
+
+void main() {
+  Student studentOne = Student(2, 2, "Du", "CSE");
+  studentOne.moving();
+  studentOne.eating();
+}
